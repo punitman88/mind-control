604a59adc246d387d223de41febf6b314fd6f30f
punitman <punit@parakhi.com>
2018-10-04 00:02:52 +0545

    add routes for signup, login and logout

--- app.js
+++ app.js
@@ -6,6 +6,18 @@ app.get("/", function(req,res){
     res.render("landing");
 });
 
+app.get("/signup", function(req,res){
+    res.render("signup");
+});
+
+app.get("/login", function(req,res){
+    res.render("login");
+});
+
+app.get("/logout", function(req,res){
+    res.redirect("/");
+});
+
 app.listen(3000, function(){
     console.log("Application now running at port 3000");
 })
